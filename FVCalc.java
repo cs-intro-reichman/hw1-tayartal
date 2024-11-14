8070b872ac7ac853c8127911c7f9354e2c2f47a2
Tal Tayar <ttayar@google.com>
2024-11-14 23:38:48 +0200

    pow

--- FVCalc.java
+++ FVCalc.java
@@ -6,8 +6,10 @@ public class FVCalc {
 		double ratePrecent = Double.parseDouble(args[1]);
 		double numberOfYears = Double.parseDouble(args[2]);
 		double rate = ratePrecent / 100;
+		System.out.println("rate " + rate);
 		double ratePlusOne = rate + 1;
-		double futureValue = currentValue * Math.pow(ratePlusOne, numberOfYears);
+		double pow = Math.pow(ratePlusOne, numberOfYears);
+		double futureValue = currentValue * pow;
 		System.out.println("After " + (int)numberOfYears + " years, $" + currentValue + " saved at " + ratePrecent + "% will yield $" + (int)futureValue);
 
 	}
